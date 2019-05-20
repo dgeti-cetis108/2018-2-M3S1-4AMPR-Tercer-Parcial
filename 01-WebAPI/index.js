b3b525395b5f71db27242c0f5de123e389d7f8fc
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-05-20 07:26:32 -0600

    Express POST Request

--- 01-WebAPI/index.js
+++ 01-WebAPI/index.js
@@ -1,12 +1,29 @@
 var express = require("express")
+var bodyParser = require("body-parser")
 var app = express()
 var port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 // app.get('/', function (req, res) {
 //   res.send('Hola Dummies!')
 // })
 
-app.use('/', express.static('dist/public'))
+// app.use('/', express.static('dist/public'))
+
+// peticiones get, post, put, delete
+// ruta get para /login (frontend)
+// ruta post para /login (backend)
+app.post('/login', (req, res) => {
+  let user = req.body.user
+  let password = req.body.password
+  if (user == 'bidkar' && password == '123') {
+    res.send(true)
+  } else {
+    res.send(false)
+  }
+})
 
 
 app.listen(port, function () {
