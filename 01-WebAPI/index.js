6fe7bad4a22eb8bf429302226b99a849b745eef4
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-05-21 10:04:53 -0600

    Login function
    + with async function
    + with promises 💍

--- 01-WebAPI/index.js
+++ 01-WebAPI/index.js
@@ -1,16 +1,19 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var port = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const app = express();
+const port = 3000;
+const db = new sqlite3.Database("db/cetis108.db");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/login", (req, res) => {
+app.post("/api/login", async (req, res) => {
   let user = req.body.user;
   let password = req.body.password;
-  if (user == "bidkar" && password == "123") {
-    res.send(true);
+  let result = await login(user, password);
+  if (result) {
+    res.send(result);
   } else {
     res.send(false);
   }
@@ -19,3 +22,20 @@ app.post("/login", (req, res) => {
 app.listen(port, function() {
   console.log(`Servidor Express Funcionando en el puerto ${port}`);
 });
+
+function login(username, userpasswd) {
+  return new Promise((resolve, reject) => {
+    // realizar la consulta
+    let query =
+      "SELECT id,name,firstname,lastname,email FROM users WHERE name=? AND passwd=?";
+    db.get(query, [username, userpasswd], (err, row) => {
+      if (err) {
+        console.error(err.message);
+        // retornar un valor
+        reject(false);
+      }
+      // retornar un valor
+      resolve(row);
+    });
+  });
+}
