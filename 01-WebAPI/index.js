a8534c1cd1a4f5dc3236eb2fab665c944380a299
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-05-24 13:35:55 -0600

    Body functions of validate user name & email
    + saveNewUser() added
    TODO: create body function
    TODO: evaluate results

--- 01-WebAPI/index.js
+++ 01-WebAPI/index.js
@@ -23,10 +23,10 @@ app.post("/api/login", async (req, res) => {
 app.post("/api/user/validate/name", async (req, res) => {
   let username = req.body.username;
   let result = await isValidUserName(username);
-  if (result) {
-    res.send({ isValid: true });
-  } else {
+  if (result.total == 1) {
     res.send({ isValid: false });
+  } else {
+    res.send({ isValid: true });
   }
 });
 
@@ -34,16 +34,32 @@ app.post("/api/user/validate/name", async (req, res) => {
 app.post("/api/user/validate/email", async (req, res) => {
   let useremail = req.body.useremail;
   let result = await isValidUserEmail(useremail);
-  if (result) {
+  if (result.total == 0) {
     res.send({ isValid: true });
   } else {
     res.send({ isValid: false });
   }
 });
 
+app.post("/api/user/new", async (req, res) => {
+  // name,passwd,firstname,lastname,email
+  let user = {};
+  user.name = req.body.user_name;
+  user.passwd = req.body.user_passwd;
+  user.firstname = req.body.user_firstname;
+  user.lastname = req.body.user_lastname;
+  user.email = req.body.user_email;
+  await saveNewUser(user);
+  // TODO: evaluate results
+});
+
 app.listen(port, function() {
   console.log(`Servidor Express Funcionando en el puerto ${port}`);
 });
+
+function saveNewUser(user) {
+  // TODO: code here
+}
 
 function login(username, userpasswd) {
   return new Promise((resolve, reject) => {
@@ -63,8 +79,25 @@ function login(username, userpasswd) {
 }
 
 function isValidUserName(username) {
-  // TODO: code here
+  return new Promise(resolve => {
+    let query = "SELECT COUNT(id) AS 'total' FROM users WHERE name=?";
+    db.get(query, [username], (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      resolve(row);
+    });
+  });
 }
+
 function isValidUserEmail(useremail) {
-  // TODO: code here
+  return new Promise(resolve => {
+    let query = "SELECT COUNT(id) AS 'total' FROM users WHERE email=?";
+    db.get(query, [useremail], (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      resolve(row);
+    });
+  });
 }
